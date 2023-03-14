88b95155abe83aacefe09dc1da1e3efaf8741998
pilyav4ik <ipiliavskyi@gmail.com>
2023-03-14 14:12:49 +0100

    The template structure was created and the icon library was added

---
+++ src/header/header.js
@@ -0,0 +1,14 @@
+import Menu from "./menu";
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                Logo
+            </div>
+            <Menu/>
+        </header>
+    )
+}
+
+export default Header;
