65c1b6ccd27209bdb592d8e99b0384ef8680cde0
pilyav4ik <ipiliavskyi@gmail.com>
2023-03-14 16:25:14 +0100

    Created style file  and added style for nav menu

--- src/header/header.js
+++ src/header/header.js
@@ -1,14 +1,18 @@
 import Menu from "./menu";
 
 const Header = () => {
-    return (
-        <header>
-            <div>
-                Logo
-            </div>
-            <Menu/>
-        </header>
-    )
-}
+  return (
+    <header>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Logo
+          </a>
+          <Menu />
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import '@/styles/globals.css'
+import '../styles/global.sass'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
